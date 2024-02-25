efc2a7be92e045b00ab825411ea90acfdaeece14
00013344wiut <aasadullokh@icloud.com>
2024-02-25 21:08:53 +0500

    Configurations of layers middleware

---
+++ WAD.13344.Application/Configurations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace WAD._13344.Application
+{
+	public static class Configurations
+	{
+		public static IServiceCollection ApplicationServices(this IServiceCollection services)
+		{
+			services.AddAutoMapper(Assembly.GetExecutingAssembly());
+			services.AddMediatR(config =>
+			{
+				config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+			});
+
+			return services;
+		}
+	}
+}
---
+++ WAD.13344.Infrastructure/Configurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WAD._13344.Application.Interfaces;
+using WAD._13344.Infrastructure.Data;
+using WAD._13344.Infrastructure.Repositories;
+
+namespace WAD._13344.Infrastructure
+{
+	public static class Configurations
+	{
+		public static IServiceCollection InfrastructureServices(
+			this IServiceCollection services,
+			IConfiguration configuration
+		)
+		{
+			services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+			services.AddScoped<IBookRepository, BookRepository>();
+			services.AddScoped<IAuthorRepository, AuthorRepository>();
+			services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+			return services;
+		}
+	}
+}
